531c27f535ed8acaf39d7e304c263ee88c26a4c6
TheRealComedian <zachary.belouchi@gmail.com>
2022-10-31 10:53:09 -0400

    Added prototype styles for results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,38 +1,48 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Results() {
     const gameData = JSON.parse(window.sessionStorage.getItem('gameData'))
     const scorePercentage = (gameData.currentScore / gameData.questionTotal)* 100
+    const nav = useNavigate()
     
   return (
     <div className="results">
-        <p>THE END</p>
+        <h1>Results!</h1>
         {/* score */}
-        <p>Score: {gameData.currentScore} ; {scorePercentage}%</p>
+        <p className='results__score'>You got {gameData.currentScore} correct answers ({scorePercentage}%)</p>
         <Message score={scorePercentage} />
+        <button variant="contained" className='results__restart btn' onClick={() => {
+            window.sessionStorage.removeItem('questions')
+            window.sessionStorage.removeItem('gameData')
+            nav('/')
+        }}>Play Again</button>
+
     </div>
+    
             
   )
 }
 
 function Message(props) {
-    const messages = {
-        terrible: "terrible",
-        poor: "poor",
-        average: "average",
-        decent: "decent",
-        great: "great"
-    }
+    const rank = useRef()
 
     if (props.score > 80) {
-        return <p>great</p>
+        rank.current = "Great"
     } else if (props.score >= 60) {
-        return <p>decent</p>
+        rank.current = "Decent"
     } else if (props.score >= 40) {
-        return <p>average</p>
+        rank.current = "Average"
     } else if (props.score >= 20) {
-        return <p>poor</p>
+        rank.current = "Poor"
     } else if (props.score < 20) {
-        return <p>terrible</p>
+        rank.current = "Terrible"
     }
+
+    return (
+        <div className="results__message">
+            <p>You did {rank.current}</p>
+        </div>
+    )
 }
